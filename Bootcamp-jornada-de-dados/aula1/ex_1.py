80c13db42f1d3ea03c8e05887c3690d1ddf15581
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-04-27 14:24:57 -0300

    KPI
    exercicio feito na aula para calculo de KPI

--- Bootcamp-jornada-de-dados/aula1/ex_1.py
+++ Bootcamp-jornada-de-dados/aula1/ex_1.py
@@ -1,3 +1,8 @@
-nome = input("qual seu nome?")
-num_nome = len(nome)
-print(len(nome))
+print("calculo de kpi 2026")
+    
+nome = input("qual seu nome? ")
+salario = float(input("qual seu salário? "))
+bonus = float(input("qual a porcentagem do bonus? "))
+
+calculo = 1000 + salario * bonus
+print(f"murilo, seu KPI é {calculo}")
