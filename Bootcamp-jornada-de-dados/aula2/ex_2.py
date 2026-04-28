dbe6e37ef900310a920e5e013f8acfaf2a0f4fab
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-04-28 11:23:02 -0300

    programa que soma dois números inteiros inseridos pelo usuário.

---
+++ Bootcamp-jornada-de-dados/aula2/ex_2.py
@@ -0,0 +1,6 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+a = int(input("digite um número: "))
+b = int(input("digite outro número: "))
+c = a + b
+print(f"a soma dos números é: {1c}")
