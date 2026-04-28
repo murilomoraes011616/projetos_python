46443f5b577dc3b0714e359d8dc23426433e3936
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-04-28 12:11:06 -0300

    teste

--- Bootcamp-jornada-de-dados/aula2/ex_2_aula2.py
+++ Bootcamp-jornada-de-dados/aula2/ex_2_aula2.py
@@ -1,5 +1,5 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
 num = float(input("digite um número: "))
-divisaoo = num/5 
-print(divisaoo)
+divisao = num/5 
+print(divisao)
