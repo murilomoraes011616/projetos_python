87ace0964b0815da324dbda1ebb054afa722c288
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-04-28 12:43:09 -0300

    teste final

--- Bootcamp-jornada-de-dados/aula2/ex_2_aula2.py
+++ Bootcamp-jornada-de-dados/aula2/ex_2_aula2.py
@@ -1,5 +1,5 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
 num = float(input("digite um número: "))
-divisao = num/5
+divisao = num/54
 print(divisao)
