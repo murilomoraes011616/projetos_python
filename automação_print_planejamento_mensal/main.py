019d9617f16b0adabee2117d9cfce3ad3d60e6d7
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-13 09:58:00 -0300

    primeira =linhas de codigo que leem o valor de uma celula dentro da planilha

--- automação_print_planejamento_mensal/main.py
+++ automação_print_planejamento_mensal/main.py
@@ -1,3 +1,6 @@
 import xlwings as xw
 wb = xw.Book(r'C:\Users\murilo.oliveira\OneDrive - Greentech\planilhas1.xlsx')
 abrir_planilha = wb.sheets('verde')
+valor_de_uma_celula = abrir_planilha.range('A1').value
+print(valor_de_uma_celula)
+breakpoint
