47410c34c466af15bcef62eb0f13e36f9f9bb12c
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-13 13:57:36 -0300

    organizando o codigo, colocando um comando para atualizar o arquvo todo depois de modificar a celula e colocando um comando de esperar 10 segudnos para poder ter certeza sobre a atualização de dados

--- automação_print_planejamento_mensal/main.py
+++ automação_print_planejamento_mensal/main.py
@@ -1,13 +1,16 @@
 import xlwings as xw # importa a biblioteca para manipular o excel .
 from datetime import date # importa sobemente a função date da biblioteca datetime de pega a data, biblioteca do python ja.
+import time # importa biblioteca para poder dar o comando de esperar 10 segundos 
+
 
 wb = xw.Book(r'C:\Users\murilo.oliveira\OneDrive - Greentech\planilhas1.xlsx') #criadno o wb, objeto da classe book.
 abrir_planilha = wb.sheets('verde') #cria uma variavel, que dentro do objeto tem o metodo sheets, que abre a planilha que estiver em seu paramtro ().
 qual_valor_de_uma_celula = abrir_planilha.range('A1').value #encontra o valor da celula A1.
 print(qual_valor_de_uma_celula) #printa esse valor.
 
-data_de_hoje = date.today()   # pega a funçãoi de hoje da biblioteca datetime e guarda esse valor me um variavel.
+data_de_hoje = date.today()   # pega a função de hoje da biblioteca datetime e guarda esse valor me um variavel.
 abrir_planilha.range('A1').value = data_de_hoje #encontra esse valor da celula A1 e modifica ele.
 qual_valor_ATUALIZADO_de_uma_celula = abrir_planilha.range('A1').value #variavel que equivale ao novo valorr.
 print(qual_valor_ATUALIZADO_de_uma_celula) #printa esse novo valor.
-wb.api.RefreshAll()
+wb.api.RefreshAll()  
+time.sleep(10) #espera 10 segundos no codigo apenas para poder para garantir a atualização dos dados 
