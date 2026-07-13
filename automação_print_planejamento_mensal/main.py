42fefdb51401b9331de40bd634330db436ba914e
Murilo Moraes <murilo.moraes011616@gmail.com>
2026-07-13 10:22:50 -0300

    atualizando e otimizando o codigo

--- automação_print_planejamento_mensal/main.py
+++ automação_print_planejamento_mensal/main.py
@@ -1,9 +1,8 @@
 import xlwings as xw
-wb = xw.Book(r'C:\Users\murilo.oliveira\OneDrive - Greentech\planilhas1.xlsx')
+wb = xw.Book(r'C:\Users\murilo.oliveira\OneDrive - Greentech\planilhas1.xlsx') #
 abrir_planilha = wb.sheets('verde')
 qual_valor_de_uma_celula = abrir_planilha.range('A1').value
 print(qual_valor_de_uma_celula)
 abrir_planilha.range('A1').value = 'Olá, Excel!'
-qual_valor_de_uma_celula = abrir_planilha.range('A1').value
-print(qual_valor_de_uma_celula)
-breakpoint
+qual_valor_ATUALIZADO_de_uma_celula = abrir_planilha.range('A1').value
+print(qual_valor_ATUALIZADO_de_uma_celula)
